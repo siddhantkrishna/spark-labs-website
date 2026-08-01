9a2e7e4762a534f1ae61ffa50ac93803c94f9729
SIddhant Krishna <siddhantkrishnaaa@gmail.com>
2026-08-01 23:56:23 +0530

    Remove unused imports from vite.config.ts
    Removed unused imports for tailwindcss and react.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
