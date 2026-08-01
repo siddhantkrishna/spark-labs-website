3f038f490a7ceafd055cd5a1f7872a7c5bdb87e2
SIddhant Krishna <siddhantkrishnaaa@gmail.com>
2026-08-01 23:56:34 +0530

    Add Tailwind CSS and React plugins to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
-
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
